cda600fe9369af7cd35f8b6328c96e86fde2cecf
Greg-Chinnici <91907134+Greg-Chinnici@users.noreply.github.com>
2022-07-09 18:39:12 -0700

    Update WhatToWatch.py

--- WhatToWatch.py
+++ WhatToWatch.py
@@ -1,8 +1,13 @@
 import webbrowser
 import random
 
+#main goal is to make a tv guide
 """
 PLAN:
+use this: https://dev.twitch.tv/docs/api/reference#get-streams
+    or
+    https://github.com/Teekeks/pyTwitchAPI
+    
     pulls the stream title, and 
     current game of specifeid streamers, with an if online check
     also gets top recommended youtube videos
@@ -10,12 +15,12 @@ PLAN:
 
 #I should make an option to just userInput the index instead of the full name
 
-streamerTwitch = ["valorant", "rocketleague", "aceu", "tarik", "iitztimmy", "daltoosh", "shahzam", "hiko", "kitboga"]
+streamerTwitch = ["valorant", "rocketleague", "playapex", "aceu", "tarik", "prod", "daltoosh", "iitztimmy", "shahzam", "hiko", "kitboga"]
 def getStreamer():
     userInput = ""
     while userInput not in streamerTwitch:
         for name in streamerTwitch:
-            print(name)
+            print(name)#this is where I want to use the isLive authenitcation
         userInput = input("What stream do you want to watch: ")
         userInput = userInput.lower()
         if userInput == "random":
@@ -25,13 +30,24 @@ def getStreamer():
 def goToTwitchLink(name):
     chrome_path = '"C:\\Program Files\\Google\\Chrome\\Application\\chrome.exe" %s' #file path to the Chrome app, otherwise it opens in IE
     webbrowser.get(chrome_path).open('twitch.tv/' + name)
-
 #TO GET TO TWITCH STREAMERS ONLY
 
 
 
-#name : actual link address
-youtubeChannels = {"Ludwig": "Ludwigahgren", "Lashmak": "Lashmak", "Linus Tech Tips": "LinusTechTips", "Tom Scott": "TomScottGo"}
+#name : actual link address , I could also add thier main video type ie: valorant, coding, bored watch
+youtubeChannels = {#had to include the "c/" and "user/" because of legacy accounts
+    "Linus Tech Tips": "c/LinusTechTips", 
+    "Tom Scott": "c/TomScottGo",
+    "Ludwig": "c/Ludwigahgren", 
+    "The Yard": "c/TheYardPodcast",
+    "Ethoslab": "user/EthosLab",
+    "Lashmak": "c/Lashmak", 
+    "direwolf20": "user/direwolf20",
+    "Binging with Babish": "c/bingingwithbabish",
+    "Sebastian Lague": "c/SebastianLague",
+    "MichaelReeves": "c/MichaelReeves",
+    "Prod Valorant": "channel/UC_L9jCsleYEyLhycNvj-6Gw"
+    }
 def getYoutuber():
     userInput = ""
     while userInput not in youtubeChannels:
@@ -46,13 +62,21 @@ def getYoutuber():
 
 def goToYoutubeLink(name):
     chrome_path = '"C:\\Program Files\\Google\\Chrome\\Application\\chrome.exe" %s' #file path to the Chrome app, otherwise it opens in IE
-    webbrowser.get(chrome_path).open('youtube.com/c/' + name)
+    webbrowser.get(chrome_path).open('youtube.com/' + name)
 #Youtube only
 
 
 
 #songname : link , timestamp can be added also just add "&t=time" (seconds) at the end
-youtubeMusic = {"Juice WRLD FreeStyle Eminem Beats": "fSoT13msPe4&t" , "Juice WRLD freestyle 60 mins": "igc1wYW448w" , "Juice WRLD Black and White": "aQDhBNHBQUs" , "Pop Smoke Shake The Room": "zd8uWVh8b8k" , "Yeat Minions" : "7fX2kMaiMGI" , "Big Rap Playlist": "60-l6nNyK70&list=PLD3MHnA3bEH-lPyn3I5XCbab-ULH8wn9N", "Trippie Red Sleepy Hallow": "1yzZwbFGLgo"}
+youtubeMusic = {
+    "Juice WRLD FreeStyle Eminem Beats": "fSoT13msPe4&t",
+    "Juice WRLD freestyle 60 mins": "igc1wYW448w" ,
+    "Juice WRLD Black and White": "aQDhBNHBQUs" , 
+    "Pop Smoke Shake The Room": "zd8uWVh8b8k" , 
+    "Yeat Minions" : "7fX2kMaiMGI" , 
+    "Big Rap Playlist": "60-l6nNyK70&list=PLD3MHnA3bEH-lPyn3I5XCbab-ULH8wn9N", 
+    "Trippie Red Sleepy Hallow": "1yzZwbFGLgo"
+    }
 def getYoutubeMusic():
     userInput = -1
     while userInput not in range(len(youtubeMusic) + 1) or userInput == 0:
@@ -72,18 +96,31 @@ def goToYoutubeMusicLink(linkchars):
 
 
 
-allChannels = []#used for the surprise me hidden feature
+def goToNetflix():
+    chrome_path = '"C:\\Program Files\\Google\\Chrome\\Application\\chrome.exe" %s' #file path to the Chrome app, otherwise it opens in IE
+    webbrowser.get(chrome_path).open("netflix.com/browse/m/continue-watching")
+#netflix
+
+
+
+allChannels = []#used for the surprise/random me hidden feature
+allChannels.extend(["netflix"])
 allChannels.extend(streamerTwitch)
-allChannels.extend(youtubeChannels.values())
-allChannels.extend(youtubeMusic.values())
+allChannels.extend(list(youtubeChannels.keys()))
+allChannels.extend(list(youtubeMusic.keys()))
 
 
 
-watching = input("What do you want to watch (youtube or twitch)?: ")#"surprise" is the secret word
-if watching == "twitch":
-    name = getStreamer()
-    goToTwitchLink(name)
-if watching == "youtube":
+
+def getAllChoice():
+    for cnt , item in enumerate(allChannels, start = 1):
+        print(str(cnt) + ": " + item)
+    userchoice = int(input("Which one do you want (index): "))
+    return allChannels[userchoice - 1]
+
+
+
+def watchingYoutube():
     vidType = ""
     while vidType != "c" and vidType != "m":
         vidType = input("channel or music (c,m): ").lower()
@@ -93,11 +130,42 @@ if watching == "youtube":
     if vidType == "m":
         linkchars = getYoutubeMusic()
         goToYoutubeMusicLink(linkchars)
-if watching == "surprise":
+
+def watchingRandom():
     randchoice = random.choice(allChannels)
     if randchoice in streamerTwitch:
         goToTwitchLink(randchoice)
-    if randchoice in youtubeChannels.values():
+    elif randchoice in youtubeChannels.values():
         goToYoutubeLink(randchoice)
-    if randchoice in youtubeMusic.values():
+    elif randchoice in youtubeMusic.values():
         goToYoutubeMusicLink(randchoice)
+    else:
+        goToNetflix()
+
+def watchingAllChoice():
+    userchoice = getAllChoice()
+    if userchoice in streamerTwitch:
+        goToTwitchLink(userchoice)
+    elif userchoice in list(youtubeChannels.keys()):
+        userchoice = youtubeChannels.get(userchoice)
+        goToYoutubeLink(userchoice)
+    elif userchoice in list(youtubeMusic.keys()):
+        userchoice = youtubeMusic.get(userchoice)
+        goToYoutubeMusicLink(userchoice)
+    else:
+        goToNetflix()
+
+
+
+watching = input("What do you want to watch (youtube, twitch, netflix)?: ").lower()#"surprise" is the secret word
+if watching == "netflix":
+    goToNetflix()
+elif watching == "twitch":
+    name = getStreamer()
+    goToTwitchLink(name)
+elif watching == "youtube":
+    watchingYoutube()
+elif watching == "random" or watching == "surprise":
+    watchingRandom()
+else:#All Choice Activates
+    watchingAllChoice()
